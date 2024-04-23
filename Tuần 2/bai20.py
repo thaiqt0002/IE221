904c10387cfbd04d9a5569b4f4f36de581b2c1f1
thaiqt0002 <thaiqt0002@gmail.com>
2024-04-23 23:17:06 +0700

    Bt tuan 3

--- Tuần 2/bai20.py
+++ Tuần 2/bai20.py
@@ -13,16 +13,25 @@ class ProductionEmployee(Employee):
         return self.basic_salary + self.num_products * 5000
 
 class OfficeEmployee(Employee):
-    def __init__(self, name, birthdate, num_workdays):
+    def __init__(self, name, birthdate, num_workdays = 0):
         super().__init__(name, birthdate)
         self.num_workdays = num_workdays
 
     def calculate_salary(self):
         return self.num_workdays * 100000
 
-#Ví dụ:
-production_employee = ProductionEmployee("John Doe", "1990-01-01", 5000000, 100)
-office_employee = OfficeEmployee("Jane Smith", "1995-05-10", 20)
+# Nhập thông tin cho ProductionEmployee
+name = input("Nhập tên: ")
+birthdate = input("Nhập ngày sinh: ")
+basic_salary = float(input("Nhập lương cơ bản: "))
+num_products = int(input("Nhập số lượng sản phẩm: "))
+production_employee = ProductionEmployee(name, birthdate, basic_salary, num_products)
 
-print("Production Employee Salary:", production_employee.calculate_salary())
-print("Office Employee Salary:", office_employee.calculate_salary())
+# Nhập thông tin cho OfficeEmployee
+name = input("Enter name: ")
+birthdate = input("Nhập ngày sinh: ")
+num_workdays = int(input("Nhập số ngày làm việc:"))
+office_employee = OfficeEmployee(name, birthdate, num_workdays)
+
+print("Lương nhân viên sản phẩm:", production_employee.calculate_salary())
+print("Lương nhân viên văn phòng:", office_employee.calculate_salary())
---
+++ Tuần 3/README.md
@@ -0,0 +1,4 @@
+
+
+- **Phần 1:** 4 bài tập liên quan đến matrix (khó đ làm nữa)
+- **Phần 2:** Làm bài tập dựa trên thư viện Sympy để giải một vài dạng toán
